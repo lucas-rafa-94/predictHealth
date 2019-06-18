b8a87a85a33770702b906d22dfa44dd8585303e6
Lucas dos Santos <lucas.rfl.santos.94@gmail.com>
2019-06-18 19:22:59 -0300

    deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,31 +8,43 @@ import {PredictHealthApiService} from './services/predict-health-api.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  inflaRim = 0;
+  inflaBexiga = 0;
+  sucesso = false;
+  erro = false;
   predictHealthService;
   constructor(private router: Router, service: PredictHealthApiService){
     this.predictHealthService = service;
   }
   onSubmit(form) {
-    const payload = ` {
-      Inputs: {
-        input1:
-          [
-            {
-              'temperatura': ` + form.temperatura + ` ,
-              'nausea': ` + form.nausea  + `,
-              'dor lombar': ` + form.lombar  + `,
-              'urinando muito': ` + form.urinando  + `,
-              'Dor ao urinar': ` + form.dorUrinando  + `,
-              'queimcao na uretra': ` + form.queimacaoUretra  + `,
-            }
-          ],
-      },
-      GlobalParameters: {
-      }
-    } `;
-    console.log(payload);
-    this.predictHealthService.submitForm(payload).subscribe((data) => {
+    this.inflaRim = 0;
+    this.inflaBexiga = 0;
+    this.sucesso = false;
+    this.erro = false
+    console.log(form);
+    this.predictHealthService.submitForm(form).subscribe((data) => {
       console.log(data);
+      this.inflaRim = data.Results.inflaBexiga[0]["Scored Probabilities"];
+      this.inflaBexiga = data.Results.inflaRim[0]["Scored Probabilities"];
+      this.sucesso = true;
+    }, (error) => {
+      console.log(error);
+      this.erro = true;
     });
+  }
+  submitSucesso(){
+    if(this.sucesso){
+      return true;
+    }else {
+      return false;
+    }
+  }
+
+  submitErro(){
+    if(this.erro){
+      return true;
+    }else {
+      return false;
+    }
   }
 }
--- src/app/services/predict-health-api.service.ts
+++ src/app/services/predict-health-api.service.ts
@@ -7,10 +7,8 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 export class PredictHealthApiService {
   constructor(private http: HttpClient) { }
   submitForm(payload) {
-    const url = 'https://ussouthcentral.services.azureml.net/workspaces/f2fc0aabc6bc41b39e6f56afd008532b/services/0e9bf66c686842a3958f9a11ae33dc5a/execute?api-version=2.0&format=swagger';
-    const apiToken = 'zXDT8DtcpmIdlyRiXAteUKAr8yrffCd6y5/RtQIReOLwFp3KZo+7842wbBLtv1zbZsrW2liuVEV8KUluw6CANQ==';
+    const url = 'https://predicthealthback.herokuapp.com/v1/predict-health';
     const headers = new HttpHeaders()
-      .set('Bearer', apiToken)
       .set('Content-Type', 'application/json');
     return this.http.post(url, payload ,{headers});
   }
